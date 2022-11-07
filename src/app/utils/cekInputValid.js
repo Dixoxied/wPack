37c80b61e733269f15da03c9ccf06580c7b6a90b
Dixoxed <dixoxied808@yahoo.com>
2022-11-07 14:04:56 +0700

    Update commonjs >> first webpack

--- src/app/utils/cekInputValid.js
+++ src/app/utils/cekInputValid.js
@@ -1,3 +1,4 @@
 const cekInputValid = (...angka) => {
   return angka.every((num) => typeof num === 'number' && !isNaN(num));
 };
+module.exports = cekInputValid;
--- src/app/utils/parseInput.js
+++ src/app/utils/parseInput.js
@@ -1,3 +1,4 @@
 const parseInput = (...input) => {
   return input.map((str) => parseInt(str));
 };
+module.exports = parseInput;
