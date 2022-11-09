ee8326299f06d42c41921c688405852c7a69195e
Dixoxied <dixoxied808@yahoo.com>
2022-11-09 10:53:00 +0700

    Gagal menggunakkan server

--- webpack.dev.js
+++ webpack.dev.js
@@ -5,11 +5,18 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = merge(config, {
   mode : 'development',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'src'),
+    },
+    compress: true,
+    port: 9000,
+    liveReload: true,
+  },
   output : {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
     clean: true,
   },
   plugins: [new MiniCssExtractPlugin()],
-  watch: true,
 });
